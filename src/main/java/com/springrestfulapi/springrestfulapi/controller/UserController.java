1b7e904bad12717c7b77530cb8d3349b39a6dee3
Sirima Insorn <sirima@n-digitaltech.com>
2024-01-14 23:19:38 +0700

    add swagger package

--- src/main/java/com/springrestfulapi/springrestfulapi/controller/UserController.java
+++ src/main/java/com/springrestfulapi/springrestfulapi/controller/UserController.java
@@ -1,12 +1,11 @@
-package com.springrestfulapi.springrestfulapi.contronller;
+package com.springrestfulapi.springrestfulapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-
-	@GetMapping("/user")
+    @GetMapping("/user")
     public String getUser() {
         return "hello user";
     }
